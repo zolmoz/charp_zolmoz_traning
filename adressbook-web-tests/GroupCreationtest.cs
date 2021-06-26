b189c0dc34aea8fea10d21a62016014ebcabc897
zolmoz <mellamoryblim@gmail.com>
2021-06-26 17:43:23 +0800

    задание 3: Созданы вспомогательные классы для аккаунта и группы, созданы вспомогательные методы для улудшения чтения кода. Использованны вспомогательные классы для  инициализации тестовых данных и для передачи этих данных во вспомогательные методы

--- adressbook-web-tests/GroupCreationtest.cs
+++ adressbook-web-tests/GroupCreationtest.cs
@@ -42,30 +42,83 @@ namespace adressbook_web_tests
         [Test]
         public void GroupCreationTest()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            OpenHomePage();
+            Login(new AccountData("admin","secret"));
+            GoToGroupPage();
+            InitGroupCreation();
+            GroupData group = new GroupData("name_1");
+            group.Header = "test";
+            group.Footer = "test";
+            FillGroupForm(group);
+            SubmitGroupCreation();
+            ReturnToGroupPage();
+            Logout();
+
+        }
+
+        private void Logout()
+        {
+            
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void ReturnToGroupPage()
+        {
+            
             driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(GroupData groupData)
+        {
+            
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("group1");
+            driver.FindElement(By.Name("group_name")).SendKeys(groupData.Name);
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("12");
+            driver.FindElement(By.Name("group_header")).SendKeys(groupData.Header);
             driver.FindElement(By.Name("group_footer")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("12");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
-           
+            driver.FindElement(By.Name("group_footer")).SendKeys(groupData.Footer);
         }
+
+        private void InitGroupCreation()
+        {
+            
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupPage()
+        {
+            
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+
+        private void OpenHomePage()
+        {
+            
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
---
+++ adressbook-web-tests/GroupData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adressbook_web_tests
+{
+    class GroupData
+    {
+        private string name;
+        private string header;
+        private string footer;
+
+
+        public GroupData(string name)
+        {
+            this.name = name;
+         
+        }
+        public GroupData(string name, string header, string footer)
+        {
+            this.name = name;
+            this.header = header;
+            this.footer = footer;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+            set
+            {
+                header = value;
+            }
+        }
+
+
+        public string Footer
+        {
+            get
+            {
+                return footer;
+            }
+            set
+            {
+                footer = value;
+            }
+        }
+
+
+    }
+}
