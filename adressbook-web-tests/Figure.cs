31e6eef1ee321c9a5a9df160ed0c4db60a492c7c
zolmoz <mellamoryblim@gmail.com>
2021-06-26 17:41:38 +0800

    Создание классов с типами фигур,  изучение связи классов и вызовов в объектах

---
+++ adressbook-web-tests/Figure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adressbook_web_tests
+{
+    class Figure
+    {
+        private bool colored = false;
+
+
+        public bool Colored
+        {
+            get
+            {
+                return colored;
+            }
+            set
+            {
+                colored = value;
+            }
+        }
+
+    }
+}
---
+++ adressbook-web-tests/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adressbook_web_tests
+{
+    class Square : Figure
+    {
+        private int size;
+       
+
+
+        public Square (int size)
+        {
+        this.size = size;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+
+
+
+    }
+}
